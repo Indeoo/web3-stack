b5c80667665b4d5e5567ac564767d6ee27132cd9
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2025-02-27 17:31:12 +0200

    Bump version: 1.6.5 → 1.6.6

--- libs/web3_wizzard_lib/setup.py
+++ libs/web3_wizzard_lib/setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r", encoding="utf-8") as file:
 
 setup(
     name='web3-wizzard-lib',
-    version='1.6.5',
+    version='1.6.6',
     py_modules=['web3_wizzard_lib'],
     packages=find_packages(),
     package_data={
