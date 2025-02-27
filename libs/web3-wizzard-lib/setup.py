3da95bdc05111ecdc060111695eec8002c867a70
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2025-02-27 17:42:34 +0200

    fix setup

--- libs/web3-wizzard-lib/setup.py
+++ libs/web3-wizzard-lib/setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r", encoding="utf-8") as file:
 
 setup(
     name='web3-wizzard-lib',
-    version='version='1.6.8',',
+    version='1.6.9',
     py_modules=['web3-wizzard-lib'],
     packages=find_packages(),
     package_data={
