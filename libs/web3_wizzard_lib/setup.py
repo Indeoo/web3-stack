eb8d9344243a1a88974f909e98ad0c11c06d07f5
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2025-02-27 17:17:32 +0200

    refactor web3 project structure

--- libs/web3_wizzard_lib/setup.py
+++ libs/web3_wizzard_lib/setup.py
@@ -4,12 +4,12 @@ with open("requirements.txt", "r", encoding="utf-8") as file:
     requirements = file.readlines()
 
 setup(
-    name='web3-wizzard-lib',
+    name='web3_wizzard_lib',
     version='1.6.4',
-    py_modules=['web3-wizzard-lib'],
+    py_modules=['web3_wizzard_lib'],
     packages=find_packages(),
     package_data={
-        'web3-wizzard-lib': ['resources/**/*'],
+        'web3_wizzard_lib': ['resources/**/*'],
     },
     install_requires=requirements,
     data_files=[('', ['requirements.txt'])],
