a55ecccd01284ca2449714f3e365013f4401ac7b
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2025-03-18 19:00:33 +0200

    simplify arguments setting

--- libs/sybil-engine/sybil_engine/utils/arguments_parser.py
+++ libs/sybil-engine/sybil_engine/utils/arguments_parser.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 
-def parse_arguments(default_password, spreadsheet_id, default_module):
+def parse_arguments():
     parser = argparse.ArgumentParser(description='Process arguments.')
 
     data_folder = 'data'
@@ -30,16 +30,16 @@ def parse_arguments(default_password, spreadsheet_id, default_module):
     parser.add_argument('--proxy_file', type=str, required=False,
                         default=os.environ.get('PROXY_FILE', f'{wallets_folder}/proxy.txt'),
                         help='a string to be processed')
-    parser.add_argument('--password', type=str, required=False, default=os.environ.get('PASSWORD', default_password),
+    parser.add_argument('--password', type=str, required=False, default=os.environ.get('PASSWORD', 'test'),
                         help='a string to be processed')
     parser.add_argument('--network', type=str, required=False, default=os.environ.get('NETWORK', 'MAIN'),
                         help='a string to be processed')
-    parser.add_argument('--module', type=str, required=False, default=os.environ.get('MODULE', default_module),
+    parser.add_argument('--module', type=str, required=False, default=os.environ.get('MODULE'),
                         help='a string to be processed')
     parser.add_argument('--cex_conf', type=str, required=False, default=os.environ.get('CEX_CONF', 'cex'),
                         help='a string to be processed')
     parser.add_argument('--spreadsheet_id', type=str, required=False,
-                        default=os.environ.get('SPREADSHEET_ID', spreadsheet_id),
+                        default=os.environ.get('SPREADSHEET_ID'),
                         help='a string to be processed')
 
     args = parser.parse_args()
@@ -55,7 +55,7 @@ def parse_arguments(default_password, spreadsheet_id, default_module):
     if '--account_csv' not in sys.argv:
         args.account_csv = os.path.join(args.wallets, 'accounts.csv')
 
-    return args
+    return vars(args)
 
 
 def parse_profile():
@@ -66,4 +66,4 @@ def parse_profile():
                         help='a string to be processed')
 
     args, unknown = parser.parse_known_args()
-    return args
+    return args.profile
--- libs/sybil-engine/sybil_engine/utils/fee_storage.py
+++ libs/sybil-engine/sybil_engine/utils/fee_storage.py
@@ -13,11 +13,4 @@ def add_fee(gas_token, transaction_price_wei):
 
 def print_fee():
     converted_dict = {k: float(from_wei_to_eth(v)) for k, v in FEE.items()}
-
-    amount = 0
-    # for token, fee in FEE.items():
-    #     price = get_binance_price(token + 'USDT')
-    #     amount = amount + float(from_wei_to_eth(fee)) * price
-
     logger.info(f"Total native fee is: {converted_dict}")
-    # logger.info(f"Approximate USD cost: {amount}")
