9db9ba132a437a6ada7001cc6e7f94b9b566b785
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2025-05-08 13:50:29 +0300

    upgrade SE

--- libs/sybil-engine/sybil_engine/utils/gas_utils.py
+++ libs/sybil-engine/sybil_engine/utils/gas_utils.py
@@ -1,19 +1,21 @@
 import functools
 
 from loguru import logger
+from sybil_engine.data.networks import get_chain_instance
 from web3 import Web3
 
 from sybil_engine.config.app_config import get_gas_prices
 from sybil_engine.domain.balance.balance_utils import from_wei_to_gwei
 from sybil_engine.utils.utils import randomized_sleeping
 
-web3_main = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth'))
+web3_main = Web3(Web3.HTTPProvider(get_chain_instance("ETH_MAINNET")["rpc"]))
 
 
 def l1_gas_price(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         l1_max_gas_price = get_gas_prices()['ETH_MAINNET']
+
         check_gas_price(web3_main, l1_max_gas_price, 'L1')
 
         return func(*args, **kwargs)
